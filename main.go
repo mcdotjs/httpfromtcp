6e04cb072f82849b066d168986ad7f04f84382bb
mcdotjs <mirectm@gmail.com>
2025-12-13 11:40:12 +0100

    read lines

--- main.go
+++ main.go
@@ -4,27 +4,37 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
-	//load once
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		fmt.Println("loading file error")
 	}
 
+	line := ""
 	for {
 		chunk := make([]byte, 8)
 
 		n, err := file.Read(chunk)
 		if err != nil {
-			if err == io.EOF{
+			if err == io.EOF {
 				break
 			}
 			break
 		}
-		// fmt.Printf("read: %s\n", string(chunk))     // "Hello!\x00\x00" ❌
-		// fmt.Printf("read: %s\n", string(chunk[:n])) // "Hello!"
-		fmt.Printf("read: %s\n", string(chunk[:n]))
+		str := string(chunk[:n])
+		parts := strings.Split(str, "\n")
+		if len(parts) == 1 {
+			line += parts[0]
+			continue
+		}
+		if len(parts) == 2 {
+			line += parts[0]
+			fmt.Printf("read: %s\n", line)
+			line = ""
+			line += parts[1]
+		}
 	}
 }
